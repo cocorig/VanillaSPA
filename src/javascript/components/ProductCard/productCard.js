77ab0e4136d686061aad4fe433a91f7c5c28ef98
Hayeon LEE <fhdodl@naver.com>
2024-05-02 00:01:23 +0900

    feat: Card 컴포넌트를 렌더링하는 ProductCard 클래스 구현

---
+++ src/javascript/components/ProductCard/productCard.js
@@ -0,0 +1,27 @@
+import { ProductImage, ProductPrice, ProductName } from "../Product/index.js";
+export default class ProductCard {
+  constructor(item) {
+    this.item = item;
+  }
+  render() {
+    const { id, productName, price, thumbnailImg, discountRate } = this.item;
+
+    // 전체 a 태그
+    const product = document.createElement("a");
+    product.setAttribute("href", `/detail/${id}`);
+    product.setAttribute("class", "product");
+
+    const productImage = new ProductImage({
+      thumbnailImg,
+      productName,
+    }).render();
+    const productNames = new ProductName(productName).render();
+    const productPrice = new ProductPrice({ price, discountRate }).render();
+
+    product.appendChild(productImage);
+    product.appendChild(productNames);
+    product.appendChild(productPrice);
+
+    return product;
+  }
+}
