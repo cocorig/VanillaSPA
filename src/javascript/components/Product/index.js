ffedc2ff8277acb8b7202477f125bf05427afe1c
Hayeon LEE <fhdodl@naver.com>
2024-05-02 02:27:17 +0900

    feat: ProductLikeButton 클래스 내보내기

--- src/javascript/components/Product/index.js
+++ src/javascript/components/Product/index.js
@@ -1,3 +1,4 @@
 export { default as ProductImage } from "./ProductImage.js";
 export { default as ProductName } from "./ProductName.js";
 export { default as ProductPrice } from "./ProductPrice.js";
+export { default as ProductLikeButton } from "./ProductLikeButton.js";
