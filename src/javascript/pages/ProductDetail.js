a00be1e8f02f7924151e82c6fa6200fde80a5935
Hayeon LEE <fhdodl@naver.com>
2024-05-16 23:05:19 +0900

    refactor: 모든 데이터를 받아온 후 props로 전달하기 위해 isLoaded 상태 추가

--- src/javascript/pages/ProductDetail.js
+++ src/javascript/pages/ProductDetail.js
@@ -1,40 +1,51 @@
 import { getProductIdData } from "../api/product.js";
 import { Component } from "../core/index.js";
-import { ProductInfo } from "../components/Product/index.js";
 import { Ir } from "../utils/index.js";
+import {
+  ProductBasicInfo,
+  ProductDetailInfo,
+} from "../components/ProductDetail/index.js";
 export default class ProductDetail extends Component {
   constructor(props) {
     super(props);
     console.log(props); // {id: '1'}
     this.state = {
       product: [],
+      isLoaded: false,
     };
     this.setDetailProduct();
   }
   async setDetailProduct() {
     const item = await getProductIdData(this.props.id);
-    this.setState({ product: item });
+    this.setState({ product: item, isLoaded: true });
   }
 
   render() {
-    console.log(this.state.product);
-    const { modDate, stockCount } = this.state.product;
-    const ProductModal = document.createElement("article");
-    ProductModal.setAttribute("class", "productModal");
+    const container = document.createElement("article");
+    container.setAttribute("class", "product-detail");
     const productDetailPagIr = new Ir({
       tag: "h1",
-      text: "전체 상세 페이지",
+      text: "상품 상세 페이지",
     }).createElement();
-    ProductModal.appendChild(productDetailPagIr);
 
-    if (modDate && stockCount !== undefined) {
-      const productInfo = new ProductInfo({
-        modDate,
-        stockCount,
-      }).render();
-      ProductModal.appendChild(productInfo);
+    const contentWrap = document.createElement("div");
+    contentWrap.setAttribute("class", "content-wrap");
+
+    // 여기서 데이터를 받아오면 props로 보내기 위해 처리
+    if (this.state.isLoaded) {
+      // 기본정보
+      const productBasicInfo = new ProductBasicInfo({
+        product: this.state.product,
+      });
+      // 상세정보
+      const productDetailInfo = new ProductDetailInfo({
+        product: this.state.product,
+      });
+      contentWrap.append(productBasicInfo.setup(), productDetailInfo.setup());
     }
 
-    return ProductModal;
+    container.append(productDetailPagIr, contentWrap);
+
+    return container;
   }
 }
