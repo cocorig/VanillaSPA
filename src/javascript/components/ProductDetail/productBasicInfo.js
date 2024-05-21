7ac064632e8e34c06c3321b8548d54ea37444d27
Hayeon LEE <fhdodl@naver.com>
2024-05-21 23:43:33 +0900

    feat: 상품 배송 정보 컴포넌트 생성

--- src/javascript/components/ProductDetail/productBasicInfo.js
+++ src/javascript/components/ProductDetail/productBasicInfo.js
@@ -1,19 +1,64 @@
 import { Component, createComponent } from "../../core/index.js";
 import { Ir } from "../../utils/index.js";
+import {
+  ProductImage,
+  ProductName,
+  ProductPrice,
+  ProductLikeButton,
+} from "../Product/index.js";
+import { OrderForm } from "../ProductOrder/index.js";
 export default class productBasicInfo extends Component {
-  constructor(props) {
-    console.log(props);
-    super(props);
-  }
   render() {
+    const { price, productName, thumbnailImg, discountRate } =
+      this.props.product;
     const BasicInfoIr = createComponent(Ir, {
       tag: "h2",
       text: "상품 기본 정보",
     });
-
     const basicInfoSection = document.createElement("section");
-    console.log(this.props.product.id);
-    basicInfoSection.append(BasicInfoIr);
+    basicInfoSection.setAttribute("class", "product-basic");
+
+    // 종류,옵션 부분
+    const productInfoContainer = document.createElement("div");
+    productInfoContainer.setAttribute("class", "product-infoContainer");
+
+    // 상품 이미지
+    const productImage = createComponent(ProductImage, {
+      thumbnailImg,
+      productName,
+    });
+
+    const productNamePriceContainer = document.createElement("div");
+    productNamePriceContainer.setAttribute(
+      "class",
+      "product-name-price-Container"
+    );
+
+    // 이름
+    const productNames = createComponent(ProductName, {
+      productName,
+      size: "l",
+    });
+    // 가격
+    const productPrice = createComponent(ProductPrice, {
+      price,
+      discountRate,
+      size: "m",
+    });
+    productNamePriceContainer.append(productNames, productPrice);
+    productInfoContainer.append(productNamePriceContainer);
+    //  주문 컴포넌트
+    const ProductOrderForm = createComponent(OrderForm, {
+      product: this.props.product,
+    });
+    // 장바구니 버튼
+    // 찜 버튼
+    basicInfoSection.append(
+      BasicInfoIr,
+      productImage,
+      productInfoContainer,
+      ProductOrderForm
+    );
     return basicInfoSection;
   }
 }
